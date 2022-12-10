dcaa486e6cc994a2657c0a4a9b619d41faf188af
Amstherdam <developer.amst@gmail.com>
2022-12-10 04:35:32 +0300

    Computer score:1 :/

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+let playerScore = 0;
+let computerScore = 0;
+let result; 
+let playerSelection = '';
 
-const options = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('.button'); 
+const playerResult = document.querySelector('#playerResult');
+const computerResult = document.querySelector('#comResult');
 
+///
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playerSelection = button.dataset.selection;
+        playRound(playerSelection);
+    });
+});
+
+//
+const choice = ['rock', 'paper', 'scissors']
 function getComputerChoice () { 
-    const choice = options[Math.floor(Math.random() * options.length)]
-    return choice;
+    const randomChoice = Math.floor(Math.random() * choice.length)
+    return choice[randomChoice];
 }
 
-
+///
 function checkWinner(playerSelection, computerSelection) { 
     if(playerSelection == computerSelection) {
         return 'Tie'
@@ -23,80 +41,32 @@ function checkWinner(playerSelection, computerSelection) {
     }
 }
 
-function playRound(playerSelection, computerSelection) { 
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == 'Player')  { 
-        return `You win ${playerSelection} beats ${computerSelection}!`
+///
+function playRound(playerSelection) { 
+    let computerSelection = getComputerChoice();
+    if(checkWinner(playerSelection, computerSelection) == 'Player') { 
+        playerScore++;
     }
-    else if ( result == 'Computer') { 
-        return `You lose ${computerSelection} beats ${playerSelection}!`
+    else if (checkWinner(playerSelection, computerSelection) == 'Computer') { 
+        computerScore++;
     }
-    else if (result == 'Tie') { 
-        return `It is tie!`
+    else { 
+        result = 'Tie !';
     }
-}
-
-function getPlayerChoice() { 
-    const getInput = false; 
-    while( getInput == false) { 
-        const choice = (prompt('Rock Paper or Scissors','')).toLowerCase();
-        if(choice == null ) { 
-            continue;
-        }
-        if(options.includes(choice)) { 
-            getInput == true; 
-            return choice
-        }
-    }
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice()
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log('-------------------------------------------------')
-        if(checkWinner(playerSelection, computerSelection) == 'Player') { 
-            playerScore++
-        }
-        else if (checkWinner(playerSelection, computerSelection) == 'Computer') { 
-            computerScore++
-        }
-    }    
-    console.log('*** Game Over ***')
-    if(playerScore > computerScore)  {
-        console.log(`Player was the winner!`)
-    }
-    else if (playerScore < computerScore) { 
-        console.log(`Computer was the winner! :(`)
-    }
-    else {
-        console.log('Tie , so close ')
-    }
-}
-
-game();
+    playerResult.textContent = playerScore;
+    computerResult.textContent = computerScore;
+    console.log(playerSelection, 'player' , computerSelection, 'computer')
+};    
 
 
 
 
+function game() {};
 
 
+/////////////////////////////////////////////////////////////////////////
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+playRound();
 
 
