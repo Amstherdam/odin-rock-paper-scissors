2b2d7ab77da7a232c843aee54fc0a8a4534f0914
Amstherdam <developer.amst@gmail.com>
2022-12-14 00:41:35 +0300

    score problem

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const modalMessage = document.getElementById('modal');
 const computerRock = document.getElementById('computerRock');
 const computerPaper = document.getElementById('computerPaper');
 const computerScissors = document.getElementById('computerScissors');
-const roundMessage = document.getElementById('roundResult');
+const roundMessage = document.getElementById('raundResult');
 ///
 
 const modal = document.getElementById('modal')
@@ -31,61 +31,49 @@ function getComputerChoice () {
 }
 
 
-function computerAnimate (computerSelection) { 
-    if(computerSelection == 'rock') {
-        computerRock.style.opacity = '1';
-        computerPaper.style.opacity = '0.5';
-        computerScissors.style.opacity = '0.5';
-    }
-    else if (computerSelection == 'paper') { 
-        computerPaper.style.opacity = '1';
-        computerScissors.style.opacity = '0.5';
-        computerRock.style.opacity = '0.5';
-    }
-    else if ( computerSelection == 'scissors') {
-        computerScissors.style.opacity = '1';
-        computerPaper.style.opacity = '0.5';
-        computerRock.style.opacity = '0.5';
-}
-}
-
 ///
 function checkWinner(playerSelection, computerSelection) { 
     
     if(playerSelection == computerSelection) {
-        return 'Tie'
+        return 'Tie';
     }
     else if (
         (playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')
     ) {
-        return 'Player'
+        return 'Player';
     }
     else { 
-        return 'Computer'
+        return 'Computer';
     }
 }
 //////
 
 
 function playRound(playerSelection) { 
+
     playerResult.textContent = playerScore;
-    computerResult.textContent = computerScore;
-    
+    computerResult.textContent = computerScore;    
+
     let computerSelection = getComputerChoice();
+
     if(checkWinner(playerSelection, computerSelection) == 'Player') { 
+        result = `You won! ${playerSelection} beats ${computerSelection}.`
         playerScore++;
-        result = `You won! ${playerSelection} beats ${computerSelection}`
     }
     else if (checkWinner(playerSelection, computerSelection) == 'Computer') { 
+        result = `You lost ! ${computerSelection} beats ${playerSelection}.`
         computerScore++;
-        result = `You lost ! ${computerSelection} beats ${playerSelection}`
     }
-    else { 
+    else if (checkWinner(playerSelection, computerSelection) == 'Tie') { 
         result = 'Tie !';
     }
-        game();
+    console.log(playerSelection, 'player', computerSelection, 'computer');
+    roundMessage.innerHTML = result;
+    
+    game();
+
     }
     
 
@@ -106,3 +94,22 @@ function game() {
 
 
 playRound();
+
+
+/* function computerAnimate (computerSelection) { 
+    if(computerSelection == 'rock') {
+        computerRock.style.opacity = '1';
+        computerPaper.style.opacity = '0.5';
+        computerScissors.style.opacity = '0.5';
+    }
+    else if (computerSelection == 'paper') { 
+        computerPaper.style.opacity = '1';
+        computerScissors.style.opacity = '0.5';
+        computerRock.style.opacity = '0.5';
+    }
+    else if ( computerSelection == 'scissors') {
+        computerScissors.style.opacity = '1';
+        computerPaper.style.opacity = '0.5';
+        computerRock.style.opacity = '0.5';
+}
+} */
